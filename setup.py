f8f3068feceb73cdc2a7b3ce7c068b38d9497d0e
macbeth322 <chrisp533@gmail.com>
2018-04-06 18:23:01 -0400

    add unit tests

--- setup.py
+++ setup.py
@@ -12,4 +12,6 @@ setup(
     install_requires=[
         'typing'
     ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 )
--- tests/test_lazy.py
+++ tests/test_lazy.py
@@ -1,22 +1,60 @@
-#!/usr/bin/env python
+import unittest
+from unittest import mock
 
-import parser_v2 as p
+from bnf.core.transform import LazySequenceTransform
 
-def square(x: str) -> int:
-    print('squaring', x)
-    return int(x) ** 2
+class TestLazySequenceTransform(unittest.TestCase):
+    def test_lazy_get(self):
+        m = mock.Mock()
+        mock_lang = mock.Mock()
+        lst = LazySequenceTransform.create(
+            [m.a, m.b, m.c],
+            mock_lang
+        )
+        self.assertEqual(m.mock_calls, [])
 
-x = p.LazySequenceTransform(
-    [
-        p.LiteralNode("5"),
-        p.LiteralNode("6"),
-        p.LiteralNode("7"),
-    ],
-    [
-        square,
-        square,
-        square
-    ],
-    None
-)
-print(x[0])
+        self.assertEqual(lst[0], m.a.transform.return_value)
+        self.assertEqual(
+            m.mock_calls,
+            [mock.call.a.transform(mock_lang)]
+        )
+
+        # test repeat gets dont cause extra transform calls
+        self.assertEqual(lst[0], m.a.transform.return_value)
+        self.assertEqual(
+            m.mock_calls,
+            [mock.call.a.transform(mock_lang)]
+        )
+
+    def test_lazy_iter(self):
+        m = mock.Mock()
+        mock_lang = mock.Mock()
+        lst = LazySequenceTransform.create(
+            [m.a, m.b, m.c],
+            mock_lang
+        )
+        self.assertEqual(m.mock_calls, [])
+
+        ilst = iter(lst)
+        self.assertEqual(m.mock_calls, [])
+
+        self.assertEqual(next(ilst), m.a.transform.return_value)
+        self.assertEqual(
+            m.mock_calls,
+            [mock.call.a.transform(mock_lang)]
+        )
+
+        # test repeat gets dont cause extra transform calls
+        self.assertEqual(lst[0], m.a.transform.return_value)
+        self.assertEqual(
+            m.mock_calls,
+            [mock.call.a.transform(mock_lang)]
+        )
+
+        self.assertEqual(
+            list(ilst),
+            [
+                m.b.transform.return_value,
+                m.c.transform.return_value
+            ]
+        )
