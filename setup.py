c7d529fcccad6e597dad20ac170bb24577a298d0
macbeth322 <chrisp533@gmail.com>
2018-09-19 11:58:48 -0400

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ test_requirements = [
 
 setup(
     name='prosodia',
-    version='0.2.5',
+    version='0.2.6',
     author='macbeth322',
     author_email='chrisp533@gmail.com',
     package_dir={'': 'src'},
